13d99aa4b49ac3d030ab9d8451cd28f792861e64
Ricardo Pereira <ricardo.pereira@buenonetworks.com.br>
2020-06-12 18:50:38 -0300

    Adicionados componentes globais

--- resources/js/module.js
+++ resources/js/module.js
@@ -1,2 +1,15 @@
 
-console.log('core carregado');
+// Retro-compatibilidade
+require('html5shiv');
+
+// Bibliotecas globais
+require('jquery');
+require('axios');
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+require('bootstrap');
+require('@fortawesome/fontawesome-free/js/all.js');
+require('datatables.net-bs4');
+require('datatables.net-responsive');
+require('select2');
+require('sweetalert2');
